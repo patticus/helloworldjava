73fbe7ea1e09fb2076ec38e256b244c8820ff49e
Patrick Johnson <pjohnson37@southhills.edu>
2019-03-19 15:49:28 -0400

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Hello World!!!!!!");
+		System.out.println("Hihihihihihihihihihi");
+		int num1 = 3;
+		int num2 = 4;
+		int sum = num1+num2;
+		System.out.println(sum);
+	}
+
+}
